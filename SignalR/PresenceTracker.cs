fc7da7a2aa693bada0747a699ea18d9dd08b0491
Rotiboy95 <polonium209@live.com>
2023-03-09 08:08:04 +0800

    End of section 17

--- SignalR/PresenceTracker.cs
+++ SignalR/PresenceTracker.cs
@@ -4,8 +4,10 @@ namespace API.SignalR
     {
         private static readonly Dictionary<string, List<string>> OnlineUsers = new Dictionary<string, List<string>>();
 
-        public Task UserConnected(string username, string connectionId)
+        public Task<bool> UserConnected(string username, string connectionId)
         {
+            bool isOnline = false;
+
             lock (OnlineUsers)
             {
                 if (OnlineUsers.ContainsKey(username))
@@ -15,26 +17,30 @@ namespace API.SignalR
                 else
                 {
                     OnlineUsers.Add(username, new List<string> { connectionId });
+                    isOnline = true;
                 }
 
-                return Task.CompletedTask;
+                return Task.FromResult(isOnline);
             }
         }
 
-        public Task UserDisconnected(string username, string connectionId)
+        public Task<bool> UserDisconnected(string username, string connectionId)
         {
+            bool isOffiline = false;
+
             lock (OnlineUsers)
             {
-                if (!OnlineUsers.ContainsKey(username)) { return Task.CompletedTask; }
+                if (!OnlineUsers.ContainsKey(username)) { return Task.FromResult(isOffiline); }
 
                 OnlineUsers[username].Remove(connectionId);
 
                 if (OnlineUsers[username].Count == 0)
                 {
                     OnlineUsers.Remove(username);
+                    isOffiline = true;
                 }
 
-                return Task.CompletedTask;
+                return Task.FromResult(isOffiline);
             }
         }
 
